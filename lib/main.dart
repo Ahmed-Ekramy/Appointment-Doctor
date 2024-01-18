9e0de2368ab041fef7062f7c4e30a98468be5a66
Ahmed Ekramy <132719533+Ahmed-Ekramy@users.noreply.github.com>
2024-01-18 05:12:11 -0900

    initial app

--- lib/main.dart
+++ lib/main.dart
@@ -12,7 +12,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Flutter Demo',
+      title: 'Flutter Appointment',
       theme: ThemeData(
         // This is the theme of your application.
         //
@@ -52,6 +52,9 @@ class MyHomePage extends StatefulWidget {
   final String title;
 
   @override
+
+
+  //fffffffffff
   State<MyHomePage> createState() => _MyHomePageState();
 }
 
