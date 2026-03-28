04d8b42c4b51c9b01362b2444f9edd244f07bdc6
ahmed <ekramy3698@gmail.com>
2026-03-28 12:06:25 +0200

    Update formatting in responsive_size.dart

--- lib/core/responsive_size.dart
+++ lib/core/responsive_size.dart
@@ -7,6 +7,8 @@ import 'package:flutter/material.dart';
 ///   16.sp   → scaled font size
 ///   12.r    → scaled radius / padding / margin
 class ResponsiveSize {
+
+
   ResponsiveSize._();
 
   // Design reference (Figma / XD base size)
