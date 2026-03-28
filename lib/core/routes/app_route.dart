02f39384f1944649704724d9390da79eb58cd050
ahmed <ekramy3698@gmail.com>
2026-03-28 14:30:43 +0200

    Add OnboardingView and update AppRoute

--- lib/core/routes/app_route.dart
+++ lib/core/routes/app_route.dart
@@ -1,16 +1,17 @@
 import 'package:doc/core/routes/route.dart';
 import 'package:flutter/material.dart';
 
+import '../../feature/onboarding/onboarding.dart';
 import '../widgets/undefined_widget.dart';
 
 
 class AppRoute {
   Route generateRoute(RouteSettings settings) {
-      switch (settings.name) {
-        case Routes.onBoarding:
-          return  MaterialPageRoute(builder: (_) => const onBoarding());
-        default:
-          return MaterialPageRoute(builder: (_) => const UndefinedPage());
-      }
-
+    switch (settings.name) {
+      case Routes.onBoarding:
+        return MaterialPageRoute(builder: (_) => const OnboardingView());
+      default:
+        return MaterialPageRoute(builder: (_) => const UndefinedPage());
+    }
+  }
   }
