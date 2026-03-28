081aaa89d1449352028c0b3c89f9e86fa93b9840
ahmed <ekramy3698@gmail.com>
2026-03-28 14:28:31 +0200

    Update main.dart to initialize DocApp with AppRoute

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,8 @@
 import 'package:doc/doc.dart';
 import 'package:flutter/material.dart';
+
+import 'core/routes/app_route.dart';
+
 void main() {
-  runApp(const DocApp());
+  runApp(DocApp(appRoute: AppRoute()));
 }
-
-
-
