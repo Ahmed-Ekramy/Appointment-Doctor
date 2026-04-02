d023cfd6be61fd3921c609df4a77fb2c2f0cba1d
ahmed <ekramy3698@gmail.com>
2026-04-02 10:24:49 +0200

    Implement API consumer and error handling in core

---
+++ lib/core/api/api_interceptors.dart
@@ -0,0 +1,9 @@
+import 'package:dio/dio.dart';
+
+class ApiInterceptor extends Interceptor {
+  @override
+  void onRequest(RequestOptions options, RequestInterceptorHandler handler) {
+    options.headers['x-api-key'] ="1e7cc8b1b3ef4bae864c0999daddc524";
+    super.onRequest(options, handler);
+  }
+}
---
+++ lib/core/api/end_ponits.dart
@@ -0,0 +1,7 @@
+class EndPoint {
+  static String baseUrl = "https://vcare.integration25.com/api";
+  static String register = "/auth/register";
+  static String login = "/auth/login";
+  static String logout = "/auth/logout";
+  static String recipesDetails ( int id) => "recipes/$id/information";
+}
